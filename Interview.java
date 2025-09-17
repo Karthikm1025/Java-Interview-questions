c592023acd736e26f2d98d0457c10620caa4a3da
Karthikm1025 <Karthikmohan1025@gmail.com>
2025-09-17 14:06:09 +0530

    Add files via upload

---
+++ Interview.java
@@ -0,0 +1,21 @@
+package ltiM;
+
+public class Interview {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		String str = "4";
+		String str1="5";
+	
+		int num1 = Integer.parseInt(str);
+		int num2 = Integer.parseInt(str1);
+		
+		
+
+				}
+				
+	}
+
+
